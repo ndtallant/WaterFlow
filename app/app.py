ddea8d5c922ff4cfce32efe2a691a3869f5fa7fd
Nick Tallant <ndtallant@uchicago.edu>
2019-12-04 10:52:46 -0600

    User form has all states

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 '''Flask app.'''
 from flask import Flask, render_template, url_for, session, redirect, request
+import json
 import datetime as dt
 import math #For fun
 
@@ -8,14 +9,22 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     '''Home page of the webapp.'''
+
+    # Form information
     state = request.form.get('state') or 'al'
     period = request.form.get('periodGroup') or 'week'
+    with open("static/states.json", "r") as f:
+        states = json.loads(f.read())
+    
+    # Actual data
     labels, p_data, w_data = get_data(state, period)
     return render_template('index.html',
+                           user_state=state,
                            period=period,
                            data_labels=labels,
                            p_data=p_data,
-                           w_data=w_data)
+                           w_data=w_data,
+                           states=states)
 
 def get_data(state, period):
     '''Looks up relevant data from HBase.'''

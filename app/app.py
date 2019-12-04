5233a76201f7d03f47b24ade344f5d66bc52583f
Nick Tallant <ndtallant@uchicago.edu>
2019-12-03 22:09:36 -0600

    Can send data from python to js

--- app/app.py
+++ app/app.py
@@ -1,20 +1,32 @@
 '''Flask app.'''
 from flask import Flask, render_template, url_for, session, redirect, request
+import datetime as dt
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    """Home page of the webapp."""
+    '''Home page of the webapp.'''
     state = request.form.get('state') or 'al'
     print(state)
-    table = True if request.form.get('table') == 'on' else False
-    print(table)
-    return render_template('index.html', table=table)
+    labels, p_data, w_data = get_data(state, period='FIX ME')
+    return render_template('index.html',
+                           data_labels=labels,
+                           p_data=p_data,
+                           w_data=w_data)
+
+def get_data(state, period):
+    '''Looks up relevant data from HBase.'''
+    today = dt.date.today()
+    # Past Week
+    labels = [str(dt.date.today() - dt.timedelta(i)) for i in range(7)]
+    p_data = list(range(7))
+    w_data = [i//2 for i in range(7)]
+    return labels, p_data, w_data
 
 @app.route('/about')
 def about():
-    """Simply renders the about page."""
+    '''Simply renders the about page.'''
     return render_template('about.html')
 
 if __name__ == '__main__':
--- app/static/js/line-chart.js
+++ app/static/js/line-chart.js
@@ -6,17 +6,19 @@ function dataFunc() {
   return {
     type: 'line',
     data: {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: data_labels, //defined in results-view
       datasets: [
         {
           label: "Precipitation",
-          data: [65, 59, 80, 81, 56, 55, 40],
+          // Get from app
+          data: p_data,  //defined in results-view
           backgroundColor: ['rgba(105, 0, 132, .2)',],
           borderColor: ['rgba(200, 99, 132, .7)',],
           borderWidth: 2},
         {
           label: "Discharge",
-          data: [28, 48, 40, 19, 86, 27, 90],
+          // Get from app
+          data: w_data, //defined in results-view
           backgroundColor: ['rgba(0, 137, 132, .2)',],
           borderColor: ['rgba(0, 10, 130, .7)',], 
           borderWidth: 2

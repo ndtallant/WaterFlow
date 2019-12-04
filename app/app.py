861bf0fa3a357ea9b158986a5be31f3361ac4458
Nick Tallant <ndtallant@uchicago.edu>
2019-12-03 19:13:46 -0600

    Added basic table with switch

--- app/app.py
+++ app/app.py
@@ -1,12 +1,16 @@
 '''Flask app.'''
-from flask import Flask, render_template, url_for, session, redirect
+from flask import Flask, render_template, url_for, session, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Home page of the webapp."""
-    return render_template('index.html')
+    state = request.form.get('state') or 'al'
+    print(state)
+    table = True if request.form.get('table') == 'on' else False
+    print(table)
+    return render_template('index.html', table=table)
 
 @app.route('/about')
 def about():

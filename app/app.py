bc9003f2d4f765cafd8805196df5c6a980a0db6b
Nick Tallant <ndtallant@uchicago.edu>
2019-12-05 01:48:09 +0000

    Hooked up app to hbase with current radio button options

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
 '''Flask app.'''
 import json
+import calendar
+import happybase
 import datetime as dt
+from collections import OrderedDict
 from flask import Flask, render_template, request
-from credentials import zookeeperHosts, zookeeperRoot, kafkaHost
-
+from credentials import zookeeperHost, zookeeperRoot, kafkaHost
 import math #For fun 
 
 app = Flask(__name__)
+hbase = happybase.Connection(
+    host=zookeeperHost,
+    port=9090,
+    table_prefix='ndtallant_serve'
+)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,7 +23,7 @@ def index():
     state = request.form.get('state') or 'al'
     period = request.form.get('periodGroup') or 'week'
     with open("static/states.json", "r") as f:
-        states = json.loads(f.read())
+        states = json.loads(f.read(), object_pairs_hook=OrderedDict)
     
     # Actual data
     labels, p_data, w_data = get_data(state, period)
@@ -30,16 +37,41 @@ def index():
 
 def get_data(state, period):
     '''Looks up relevant data from HBase.'''
-    dates = get_dates(period)
-    p_data = list(range(len(dates)))
-    w_data = [math.sqrt(i) for i in range(len(dates))]
+    table_name, dates = get_dates(period)
+    table = hbase.table(table_name)
+    data = table.rows(['{}-{}'.format(state, date) for date in dates])
+    p_data, w_data = parse_data(data)
     return dates, p_data, w_data
+
+def parse_data(data):
+    '''Given hbase data, returns averages.'''
+    p_data, w_data = list(), list()
+    for date, d in data:
+        p_data.append(float(d[b'water:precip']) / float(d[b'water:n_precip']))
+        w_data.append(float(d[b'water:discharge']) / float(d[b'water:n_discharge']))
+    return p_data, w_data 
 
 def get_dates(period):
     today = dt.date.today()
-    dates = {'week': 6, 'month': 27, 'year': 364}
-    return [str(today - dt.timedelta(i)) for i in range(dates[period], -1, -1)]
 
+    # Past week
+    if period == 'week': 
+        return 'daily', [str(today - dt.timedelta(i)) for i in range(6, -1, -1)]
+
+    if period == 'month': 
+        this_week = today.isocalendar()[1]
+        return 'weekly', [str(this_week - i)+str(today.year) for i in range(5, -1, -1)]
+
+    # Past year
+    if period == 'year':
+        return 'monthly', [date_helper(today.month, today.year, i)\
+                           for i in range(11, -1, -1)] 
+
+def date_helper(this_month, this_year, i):
+    rv = this_month - i
+    if rv < 1:
+        return str(rv + 12) + str(this_year - 1)
+    return str(rv) + str(this_year)
 
 @app.route('/about')
 def about():
